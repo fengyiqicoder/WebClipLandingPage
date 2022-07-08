3a8ebf807a0005b937dcfd9c612846e7e42476e3
FengYQ <329650437@qq.com>
2022-07-08 11:41:19 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    darkMode: 'class',
+    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
